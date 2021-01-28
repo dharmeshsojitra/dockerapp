dcf4f3f7750bf61ed6eae7427ce848a109dd39d3
Dharmesh Sojitra <dharmesh.sojitra@fnb.co.za>
2021-01-28 21:17:20 +0530

    added logs

--- src/main/java/com/exapmle/docker/demo/HelloController.java
+++ src/main/java/com/exapmle/docker/demo/HelloController.java
@@ -1,5 +1,6 @@
 package com.exapmle.docker.demo;
 
+import jdk.internal.jline.internal.Log;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,18 +13,21 @@ public class HelloController {
 
     @RequestMapping("/hello/{name}")
     String sayHelloWorld(@PathVariable String name) {
+        Log.info("called sayHelloWorld with path variable:"+ name);
         return "Hello " + name + ", welcome to Demo! Hurray!!!";
     }
 
     //hello Docker method
     @RequestMapping("/docker/{name}")
     String sayHelloDocker(@PathVariable String name) {
+        Log.info("called sayHelloDocker with path variable:"+ name);
         return "Hello " + name + ", welcome to the docker world!";
     }
     
     //hello Openshift method
     @RequestMapping("/openshift/{name}")
     String sayHelloOpenshift(@PathVariable String name) {
+        Log.info("called sayHelloOpenshift with path variable:"+ name);
         return "Hello " + name + ", welcome to the Openshitf world!";
     }
 }
