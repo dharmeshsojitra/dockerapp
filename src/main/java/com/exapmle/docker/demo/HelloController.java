ffba3468f3e1c18d0fcaf81d802cb6b12d5a7053
Dharmesh Sojitra <dharmesh.sojitra@fnb.co.za>
2021-01-28 21:14:56 +0530

    added lombok for logs

--- src/main/java/com/exapmle/docker/demo/HelloController.java
+++ src/main/java/com/exapmle/docker/demo/HelloController.java
@@ -1,13 +1,17 @@
 package com.exapmle.docker.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Slf4j
 public class HelloController {
     @RequestMapping("/hello/{name}")
     String sayHelloWorld(@PathVariable String name) {
+
         return "Hello, " + name + "!";
     }
 
