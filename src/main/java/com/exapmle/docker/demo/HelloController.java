493ad37c6f97ef4afda9550fe7908a4aae5e917b
dharmeshsojitra <sojitra_d@yahoo.co.in>
2021-05-13 19:11:30 +0530

    Update HelloController.java
    removed slf4j

--- src/main/java/com/exapmle/docker/demo/HelloController.java
+++ src/main/java/com/exapmle/docker/demo/HelloController.java
@@ -1,32 +1,27 @@
 package com.exapmle.docker.demo;
 
-
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Slf4j
 public class HelloController {
 
     @RequestMapping("/hello/{name}")
     String sayHelloWorld(@PathVariable String name) {
-        log.info("called sayHelloWorld with path variable:"+ name);
+        System.out.println("Serving hello request");
         return "Hello " + name + ", welcome to Demo! Hurray!!!";
     }
 
     //hello Docker method
     @RequestMapping("/docker/{name}")
     String sayHelloDocker(@PathVariable String name) {
-        log.info("called sayHelloDocker with path variable:"+ name);
         return "Hello " + name + ", welcome to the docker world!";
     }
     
     //hello Openshift method
     @RequestMapping("/openshift/{name}")
     String sayHelloOpenshift(@PathVariable String name) {
-        log.info("called sayHelloOpenshift with path variable:"+ name);
         return "Hello " + name + ", welcome to the Openshitf world!";
     }
 }
