f2a2676eba9c4343075dba8e4327085d3a1aad6a
dharmeshsojitra <sojitra_d@yahoo.co.in>
2021-05-19 19:19:36 +0530

    Update HelloController.java
    added logging

--- src/main/java/com/exapmle/docker/demo/HelloController.java
+++ src/main/java/com/exapmle/docker/demo/HelloController.java
@@ -1,15 +1,27 @@
 package com.exapmle.docker.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private static final Logger LOG = LoggerFactory.getLogger(HelloController.class);
 
     @RequestMapping("/hello/{name}")
     String sayHelloWorld(@PathVariable String name) {
-        System.out.println("Serving hello request");
+        LOG.info("Original name:"+name);
+        LOG.debug("Original name:"+name);
+        LOG.error("Original name:"+name);
+        LOG.trace("Original name:"+name);
+        name = StringUtils.capitalize(name);
+        LOG.info("Name after capitalise:"+name);
+        LOG.debug("Name after capitalise:"+name);
+        LOG.error("Name after capitalise:"+name);
+        LOG.trace("Name after capitalise:"+name);
         return "Hello " + name + ", welcome to Demo! Hurray!!!";
     }
 
